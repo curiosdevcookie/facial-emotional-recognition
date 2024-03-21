76dda9d077b33eeed6161ffc5f938cf97cd80b96
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2024-03-21 09:02:19 +0100

    Add face detection in the captured video feed.

--- emotion_recognition.py
+++ emotion_recognition.py
@@ -1,14 +1,21 @@
 import cv2
 
 def main():
-    capture = cv2.VideoCapture(1)
+    capture = cv2.VideoCapture(0)
 
     while True:
         ret, frame = capture.read()
         if not ret:
             break
 
-        cv2.imshow('Video Feed', frame)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(500, 500))
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            cv2.imshow('Video Feed', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
