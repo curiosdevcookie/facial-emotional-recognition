f5a41eeba6c37afa3e28e1742e7296b3a5fef776
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2024-03-21 10:13:51 +0100

    Add error handling for loading the CascadeClassifier.

--- emotion_recognition.py
+++ emotion_recognition.py
@@ -11,7 +11,13 @@ def main():
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        try:
+            face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+            if face_cascade.empty():
+                raise IOError("Failed to load haarcascade for face detection.")
+        except Exception as e:
+            print(e)
+            exit(1)
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(500, 500))
 
         for (x, y, w, h) in faces:
